62f8c508779bfc9e22fbdd59668683d5112fbcae
Ali HD <85040609+alihd-tech@users.noreply.github.com>
2024-01-25 15:25:58 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '15f835d3-b746-4533-a9bd-66cda2f15e1b';
+let userID = '388080c4-2bfa-4921-ad40-f8ca1047e093';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
